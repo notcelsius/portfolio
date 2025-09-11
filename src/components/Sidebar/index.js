cf01940c5f17321f4f60ae846cf546db4b40f49f
notcelsius <149901647+notcelsius@users.noreply.github.com>
2025-09-11 00:18:42 -0700

    Finished the sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,8 @@ import {Link, NavLink} from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHome} from '@fortawesome/free-solid-svg-icons'
+import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -12,11 +13,33 @@ const Sidebar = () => (
             <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
         </ Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            {/* exact ensures link is only active if current url matches exactly
+                activeclassname allows me to style the active link differently
+                like highliting a home icon when on home page
+                to tells the react router where the link should go when clicked*/}
+            <NavLink exact="true" activeclassname="active" to="/"> 
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about"> 
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact"> 
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/kelvin-trinh-93049a267/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/notcelsius'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
