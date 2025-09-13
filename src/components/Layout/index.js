0846ab6781c1c395078527587290f0ff485fcea6
notcelsius <149901647+notcelsius@users.noreply.github.com>
2025-09-13 14:16:08 -0700

    Building site layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,24 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar'
 import './index.scss';
 
 const Layout = () => {
     return (
-    <>
-        <Sidebar />
-    </>
+    <div className="App">
+        <Sidebar /> {/* renders the sidebar */}
+        <div className='page'>
+            <span className='tags top-tags'>&lt;body&gt;</span>
+
+            <Outlet />  {/* placeholder for where the current route's content gets inserted */}
+
+            <span className='tags bottom-tags'>
+                &lt;/body&gt;
+                <br />
+                <span className='bottom-tag-html'>&lt;/html&gt;</span>
+
+            </span>
+        </div>
+    </div>
     )
 }
 
