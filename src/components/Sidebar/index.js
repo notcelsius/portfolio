90dae8d978b74fb749165fb96613e0f3a3677807
notcelsius <149901647+notcelsius@users.noreply.github.com>
2025-09-20 00:51:05 -0700

    Added a portfolio page

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import {Link, NavLink} from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faHome, faUser, faEnvelope, faFolder} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
@@ -23,9 +23,13 @@ const Sidebar = () => (
             <NavLink exact="true" activeclassname="active" className="about-link" to="/about"> 
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
+            <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio"> 
+                <FontAwesomeIcon icon={faFolder} color="#4d4d4e" />
+            </NavLink>
             <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact"> 
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
+            
         </nav>
         <ul>
             <li>
